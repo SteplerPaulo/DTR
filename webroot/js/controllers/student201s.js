c0be05a462681498d41b6aabc4910c8c9e42bf30
Paulo <paulobiscocho>
2016-12-08 17:57:54 +0800

    Clean Student 201 JS

--- webroot/js/controllers/student201s.js
+++ webroot/js/controllers/student201s.js
@@ -3,12 +3,8 @@ App.controller('Student201Controller',function($scope,$rootScope,$http,$filter){
 	$scope.initializeController = function(){
 		$scope.currentPage = 1; 
 		$scope.pageSize = 7;
-		
-		
 		$http.get("/DTR/student201s/all").success(function(response) {
-		
 			$scope.students = response;
-			console.log(response);
 		});
 	}
 });
