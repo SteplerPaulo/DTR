cb5dca1fd05fc8be8068f3fcd0e8eea4def4d32b
Paulo <paulobiscocho>
2017-02-14 12:27:12 +0800

    Mobile IF Null

--- webroot/js/controllers/assign.js
+++ webroot/js/controllers/assign.js
@@ -109,18 +109,11 @@ App.controller('AssignRFIDController',function($scope,$rootScope,$http,$filter){
 							$scope.Field.last_name = d.Student201.last_name;
 							$scope.Field.first_name = d.Student201.first_name;
 							$scope.Field.middle_name = d.Student201.middle_name;
-							$scope.Field.student_mobile_no = d.Student201.mobile.substring(3, 13);
-							$scope.Field.guardian_mobile_no = d.Student201.primary_mobile_no.substring(3, 13);
+							$scope.Field.student_mobile_no = (d.Student201.mobile != null)?d.Student201.mobile.substring(3, 13):'';
+							$scope.Field.guardian_mobile_no = (d.Student201.primary_mobile_no != null)?d.Student201.primary_mobile_no.substring(3, 13):'';
 							$scope.Field.relationship = d.Student201.primary_relationship;
 							$scope.Field.level_id = d.Student201.level_id;
 							$scope.Field.section_id = d.Student201.section_code;
-							
-							
-							console.log(d.Student201);
-							
-							
-							
-							
 							
 						}else{			//IF NO DATA FOUND
 							$scope.No201=true;
