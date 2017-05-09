34216169ed2838b6a69aa6e57f18f7acac5885ce
Paulo <paulobiscocho>
2017-05-09 20:16:44 +0800

    ASSOCIATE RFID STUDENT TO LEVEL & SECTION

--- models/rfid_student.php
+++ models/rfid_student.php
@@ -25,6 +25,23 @@ class RfidStudent extends AppModel {
 									END "
 	);
 
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	var $belongsTo = array(
+		'Level' => array(
+			'className' => 'Level',
+			'foreignKey' => 'level_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Section' => array(
+			'className' => 'Section',
+			'foreignKey' => 'section_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
 
 	var $hasAndBelongsToMany = array(
 		'Fetcher' => array(
@@ -43,5 +60,44 @@ class RfidStudent extends AppModel {
 			'insertQuery' => ''
 		)
 	);
+	
+	
+	function reset_data(){
+		return $this->query( 
+			"SELECT 
+			  `rfid_students`.`student_number`,
+			  CONCAT(
+				IFNULL(`rfid_students`.`last_name`, ''),
+				', ',
+				IFNULL(
+				  `rfid_students`.`middle_name`,
+				  ''
+				),
+				' ',
+				IFNULL(
+				  `rfid_students`.`first_name`,
+				  ''
+				)
+			  ) AS full_name,
+			  `rfid_students`.`source_rfid`,
+			  `rfid_students`.`level_id`,
+			  `rfid_students`.`section_id`,
+			  levels.name,
+			  levels.alias,
+			  sections.id,
+			  sections.name ,
+			  sections.level
+			FROM
+			  `gatekeeper`.`sections` 
+			  INNER JOIN `gatekeeper`.`levels` 
+				ON (
+				  `sections`.`level` = `levels`.`alias`
+				) 
+			  INNER JOIN `gatekeeper`.`rfid_students` 
+				ON (
+				  `rfid_students`.`section_id` = `sections`.`id`
+				)ORDER BY full_name ASC;
+			");
+	}
 
 }
