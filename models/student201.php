cf60e9ab24ec0528bfa2fb32f107c18c4b839c17
Paulo <paulobiscocho>
2018-09-20 13:09:46 +0800

    Student 201 Association

--- models/student201.php
+++ models/student201.php
@@ -18,5 +18,21 @@ class Student201 extends AppModel {
 									END "
 	);
 	
-
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	var $belongsTo = array(
+		'Level' => array(
+			'className' => 'Level',
+			'foreignKey' => 'level_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Section' => array(
+			'className' => 'Section',
+			'foreignKey' => 'section_code',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
 }
